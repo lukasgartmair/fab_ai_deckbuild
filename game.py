2d4a540fae55f49d693c2b69898e3cbc4b922d9e
lukasgartmair <lukas.gartmair@gmail.com>
2024-08-11 13:13:11 +0200

    added files

---
+++ game.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 11 08:41:26 2024
+
+@author: lukasgartmair
+"""
+
+import sys
+import pygame
+from renderer import Renderer
+from engine import GameEngine
+from settings import FPS
+
+
+class Game:
+    def __init__(self):
+        self.engine = GameEngine()
+        pygame.display.set_caption("Fight!")
+
+        self.renderer = Renderer(self.engine)
+
+    @staticmethod
+    def quit_everything(active_scene=None):
+        if active_scene:
+            active_scene.terminate()
+        pygame.display.quit()
+        pygame.quit()
+
+        sys.exit()
+
+    def run(self, starting_scene):
+        clock = pygame.time.Clock()
+
+        active_scene = starting_scene
+
+        while active_scene is not None:
+            events = pygame.event.get()
+
+            active_scene.process_input(events)
+            active_scene.update()
+            active_scene.render()
+
+            active_scene = active_scene.next
+
+            pygame.display.flip()
+            clock.tick(FPS)
---
+++ title_scene.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 27 09:15:03 2023
+
+@author: lukasgartmair
+"""
+
+import pygame
+from scene_base import SceneBase
+import scene_manager
+from game import Game
+
+
+class TitleScene(SceneBase):
+    def __init__(self, *kargs):
+        super().__init__(*kargs)
+        print("Title Scene")
+        self.skip = False
+
+    def process_input(self, events):
+        if self.skip:
+            self.switch_to_scene(
+                scene_manager.get_game_scene(self.engine, self.renderer)
+            )
+
+        for event in events:
+            if event.type == pygame.QUIT:
+                Game.quit_everything(self)
+
+            if (event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN) or (
+                event.type == pygame.MOUSEBUTTONDOWN and event.button == 1
+            ):
+                self.switch_to_scene(
+                    scene_manager.get_game_scene(self.engine, self.renderer)
+                )
+                self.engine.state_machine.start_game()
+                self.is_active = False
+
+    def get_scene_data(self):
+        pass
+
+    def update(self):
+        pass
+
+    def render(self):
+        if self.is_active:
+            self.renderer.render_start_screen()
+
+            pygame.display.flip()
