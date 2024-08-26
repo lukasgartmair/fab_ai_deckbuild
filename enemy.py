75ca809ea281826f4cb099aa578da70e70aed8bf
lukasgartmair <lukas.gartmair@gmail.com>
2024-08-26 15:15:24 +0200

    boost only with chance p

--- enemy.py
+++ enemy.py
@@ -345,7 +345,10 @@ class Enemy:
 
     def apply_mechanologist_boost_mechanic(self, card):
         if Keyword.boost in card.keywords:
-            if len(self.deck.cards) >= 2:
+            p = (len(self.hand) + len(self.arsenal)) / (
+                self.intellect + len(self.arsenal)
+            ) + 0.25
+            if n_chance(p):
                 banished_card = self.deck.draw_top_cards(n=1)
                 self.banished_zone["boosted_cards"].append(banished_card)
                 if banished_card.card_class == PlayerClass.mechanologist:
